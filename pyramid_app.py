378e26ff153f1964c108d8d7be358c9dd896c9b8
odinakholiqov <bobojon.kholikov@gmail.com>
2023-11-06 19:01:13 +0500

    WSGI class is working

---
+++ pyramid_app.py
@@ -0,0 +1,17 @@
+from pyramid.config import Configurator
+from pyramid.response import Response
+
+def root(request):
+    return Response("""
+                    <body><h1>
+                        Hello, World!
+                    </h1></body>
+                    """)
+
+
+
+config = Configurator()
+
+config.add_route("hello", "/")
+config.add_view(root, route_name="hello")
+app = config.make_wsgi_app()
